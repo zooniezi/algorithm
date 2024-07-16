f349c8a4adea3bf5b0ab50df0eb2d4b79b104041
JaeJun Kim <75923015+zooniezi@users.noreply.github.com>
2024-07-16 15:40:48 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1000001];
+
+int ans(int n)
+{
+	for(int i=2; i<=n; i++)
+	{
+		dp[i] = dp[i-1]+1;
+		if(i%2 ==0)
+		{
+			dp[i]= min(dp[i/2]+1, dp[i]);
+		}
+		if(i%3==0)
+		{
+			dp[i]= min(dp[i/3]+1, dp[i]);
+		}
+		
+	}
+	return dp[n];
+}
+int main()
+{
+	dp[1]=0;
+	int t;
+	cin>>t;
+	cout<<ans(t)<<endl;
+	
+}
