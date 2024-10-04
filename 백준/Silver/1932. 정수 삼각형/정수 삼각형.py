692d7d99939a0a00495ba4627785a7d857d43dca
JaeJun Kim <75923015+zooniezi@users.noreply.github.com>
2024-10-04 10:55:56 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 41688 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,26 +1,18 @@
 import sys
 input = sys.stdin.readline
-
 n = int(input())
-triangle = []
-
-for i in range(n):
-    triangle.append(list(map(int,input().split())))
-
-max_sum = [[0 for j in range(i+1)] for i in range(n)]
-
-for i in range(n):
-    for j in range(i+1):
-        if i == 0:
-            max_sum[i][j] = triangle[0][0]
-            continue
-        else:
-            if j == 0:
-                max_sum[i][j] = triangle[i][j]+max_sum[i-1][j]
-                continue
-            if j == i:
-                max_sum[i][j] = triangle[i][j]+max_sum[i-1][j-1]
-                continue
-            max_sum[i][j] = max(triangle[i][j]+max_sum[i-1][j],triangle[i][j]+max_sum[i-1][j-1])
-
-print(max(max_sum[n-1]))
+triangle = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = triangle[0][0]
+for i in range(1, n):
+    dp[i][0] = dp[i-1][0] + triangle[i][0]
+    dp[i][i] = dp[i-1][i-1] + triangle[i][i]
+if n >=2:
+    for i in range(2, n):
+        for j in range(1, i):
+            dp[i][j] = triangle[i][j] + max(dp[i-1][j-1], dp[i-1][j])
+    result = max(dp[n-1])
+    print(result)
+else:
+    result = max(dp[n-1])
+    print(result)
