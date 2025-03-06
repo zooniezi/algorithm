fefc79c1b069304804cb3f7fadf9571b0fb6aa8e
JaeJun Kim <75923015+zooniezi@users.noreply.github.com>
2025-03-06 13:57:11 +0900

    [Bronze II] Title: 근우의 다이어리 꾸미기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/16676. 근우의 다이어리 꾸미기/근우의 다이어리 꾸미기.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+if n<11:
+    print(1)
+elif n<111:
+    print(2)
+elif n<1111:
+    print(3)
+elif n<11111:
+    print(4)
+elif n<111111:
+    print(5)
+elif n<1111111:
+    print(6)
+elif n<11111111:
+    print(7)
+elif n<111111111:
+    print(8)
+elif n<1111111111:
+    print(9)
